bc0c7c725ac6fd6e3c09d30ada3f2201de7c2770
Dominik Moritz <domoritz@gmail.com>
2019-11-09 18:37:24 -0800

    Add my papers and search (needs more styling)

---
+++ _includes/pubfilter.js
@@ -0,0 +1,213 @@
+(function() {
+  var pubElems = document.querySelectorAll(".publication");
+  var yearElems = document.querySelectorAll(".year");
+
+  var clearElem = document.getElementById("clear-filters");
+  var highlightElem = document.getElementById("highlight");
+
+  var data = [];
+  var allYears = {};
+
+  pubElems.forEach(function(element) {
+    var item = JSON.parse(element.getAttribute("data-pub"));
+
+    if (item.highlight) {
+      item.highlight = "Yes";
+    }
+
+    allYears[item.year] = 1;
+
+    item.element = element;
+
+    item.authors = (item.authors || []).filter(function(d) {
+      return d !== "Dominik Moritz";
+    });
+
+    data.push(item);
+  });
+
+  var engine = itemsjs(data, {
+    aggregations: {
+      venue_tags: {
+        size: 5
+      },
+      authors: {
+        size: 6
+      },
+      awards: {
+        size: 5
+      },
+      highlight: {
+        size: 1
+      },
+      tags: {
+        size: 6
+      },
+      type: {
+        size: 5
+      }
+    }
+  });
+
+  var query = { filters: {} };
+
+  function setAggs(aggs) {
+    document.querySelectorAll("#facets > .facet").forEach(function(facet) {
+      var id = facet.getAttribute("id");
+
+      var buckets = aggs[id].buckets;
+
+      var el = facet.querySelector("ul");
+      if (buckets.length === 0) {
+        el.innerHTML = "Empty";
+      } else {
+        el.innerHTML = "";
+
+        buckets.forEach(function(bucket) {
+          if (query.filters[id] && query.filters[id].indexOf(bucket.key) >= 0) {
+            bucket.in_query = true;
+          }
+        });
+
+        var maxDocCount = Math.max.apply(
+          null,
+          buckets.map(function(bucket) {
+            return bucket.doc_count;
+          })
+        );
+
+        buckets.forEach(function(bucket) {
+          var child = document.createElement("li");
+          child.classList.add("mb2", "pointer");
+
+          var wrap = document.createElement("span");
+          child.appendChild(wrap);
+
+          var text = document.createElement("span");
+          text.innerText = bucket.key;
+          text.setAttribute("title", bucket.key);
+          var number = document.createElement("span");
+          number.classList.add("gray", "f6");
+          number.innerText = " (" + bucket.doc_count + ")";
+          wrap.appendChild(text);
+          wrap.appendChild(number);
+
+          var barFull = document.createElement("div");
+          barFull.classList.add("w-100", "bb", "b--black-20", "bw1", "mt1");
+          child.append(barFull);
+
+          var bar = document.createElement("div");
+          bar.classList.add("bb", "b--cmu-red", "bw1");
+          bar.style.marginBottom = "-.125rem";
+          bar.style.width = "" + (bucket.doc_count / maxDocCount) * 100 + "%";
+          barFull.append(bar);
+
+          if (bucket.in_query) {
+            child.classList.add("b");
+
+            // remove filter
+            child.onclick = function() {
+              query.filters[id].splice(
+                query.filters[id].indexOf(bucket.key),
+                1
+              );
+              if (query.filters[id].length === 0) {
+                delete query.filters[id];
+              }
+              search(query);
+            };
+          } else {
+            // add to filter
+            child.onclick = function() {
+              if (query.filters[id]) {
+                query.filters[id].push(bucket.key);
+              } else {
+                query.filters[id] = [bucket.key];
+              }
+              search(query);
+            };
+          }
+
+          el.appendChild(child);
+        });
+      }
+    });
+  }
+
+  // full text search is broken
+  // var ftSearch = document.getElementById("ft-search");
+  // ftSearch.oninput = function() {
+  //   var val = ftSearch.value;
+
+  //   if (val) {
+  //     query.query = val;
+  //   } else {
+  //     delete query.val;
+  //   }
+
+  //   console.log(query);
+  //   search(query);
+  // }
+
+  function search(query) {
+    console.time("Search");
+
+    var result = engine.search(Object.assign({ per_page: 1000 }, query));
+
+    setAggs(result.data.aggregations);
+
+    var counter = pubElems.length - result.data.items.length;
+
+    document.getElementById("count_hidden").innerText = counter;
+    document.getElementById("count_total").innerText = pubElems.length;
+
+    pubElems.forEach(function(element) {
+      element.classList.add("dn");
+    });
+
+    var visibleYears = {};
+    result.data.items.forEach(function(item) {
+      item.element.classList.remove("dn");
+      visibleYears[item.year] = 1;
+    });
+
+    yearElems.forEach(function(element) {
+      element.classList.add("dn");
+    });
+    Object.keys(allYears).forEach(function(year) {
+      if (year in visibleYears) {
+        document.getElementById("y" + year).classList.remove("dn");
+      }
+    });
+
+    // show or hide notification about filtered papers
+    if (Object.keys(query.filters).length || query.query) {
+      clearElem.classList.remove("dn");
+    } else {
+      clearElem.classList.add("dn");
+    }
+
+    highlightElem.checked = !!query.filters.highlight;
+
+    console.timeEnd("Search");
+  }
+
+  highlightElem.onchange = function() {
+    if (highlightElem.checked) {
+      query.filters.highlight = ["Yes"];
+    } else {
+      delete query.filters.highlight;
+    }
+    search(query);
+  };
+
+  clearElem.onclick = function() {
+    query = { filters: {} };
+    search(query);
+  };
+
+  search(query);
+
+  document.getElementById("facets").classList.remove("dn");
+  document.getElementById("only-highlight").classList.remove("dn");
+})();
--- _publications/README.md
+++ _publications/README.md
@@ -5,12 +5,13 @@ Create a new `*.html` file in the `_publications` directory. Pick a short key as
 ```html
 ---
 layout: publication
+year: ...
 title: ...
 ...
 ---
 Abstract
 ```
 
-Add the other properties (title, authors, etc). Take for example the Draco paper page.
+Add the other properties (title, year, authors, etc). Take for example the Draco paper.
 
 TODO: add more details here similar to https://github.com/visdesignlab/visdesignlab.github.io/blob/master/_publications/README.md
